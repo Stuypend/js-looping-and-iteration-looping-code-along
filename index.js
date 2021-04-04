5a2e64c8a9b37f7c5f61790c3d4748fc3c565974
stuypend <andre@stuypend.com>
2021-04-04 09:36:34 -0400

    commit tree 5c34e129df6313472b17c6e01df2c0ed92e77156

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+
+function  writeCards(name)
+{
+  const names = [];
+  for(let i = 0; i < name.length; i++)
+  {
+    names.push("Thank you, " + name[i] + ", for the wonderful surprise gift!");
+  }
+  return names;
+}
+
+function countDown(num)
+{
+  for(let i = num; i > -1; i--)
+  {
+    console.log(i);
+  }
+}
+
+
+
+
